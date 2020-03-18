6895e8a671270b32b6749f03ee58251715fd96a1
Sondos <sondosissa18@gmail.com>
2020-03-18 05:47:02 -0500

    finished

--- hanoi.js
+++ hanoi.js
@@ -1,16 +1,11 @@
 let disc4 = document.getElementById("disc4");
-// document.getElementById("towerDiv1").appendChild(disc4);
 
 let disc3 = document.getElementById("disc3");
-// document.getElementById("towerDiv1").appendChild(disc3);
 
 let disc2 = document.getElementById("disc2");
-// document.getElementById("towerDiv1").appendChild(disc2);
 
 let disc1 = document.getElementById("disc1");
-// document.getElementById("towerDiv1").appendChild(disc1);
 
-//click handler added to each tower
 document.getElementById("tower1").addEventListener("click", whichTower);
 document.getElementById("tower2").addEventListener("click", whichTower);
 document.getElementById("tower3").addEventListener("click", whichTower);
@@ -48,13 +43,4 @@ function checkWinner() {
   console.log(document.getElementById("tower3").lastElementChild);
   let lasttower = document.getElementById("tower3");
   if (lasttower.childElementCount === 4) alert("You Won!");
-  //   if (
-  //     document.getElementById("tower1").lastElementChild === null &&
-  //     document.getElementById("tower2").lastElementChild === null &&
-  //     document.getElementById("tower3").lastElementChild ===
-  //       document.getElementById("disc1")
-  //   ) {
-  //     console.log("You Won!");
-  //     alert("You Won!");
-  //   }
 }
